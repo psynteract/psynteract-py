ede47b64d892878e21178aeda2a16f24db59567b
Felix Henninger <mailbox@felixhenninger.com>
2016-03-14 23:43:09 +0100

    Add tentative support for installation via pip

---
+++ setup.py
@@ -0,0 +1,70 @@
+"""Python Bindings for Psynteract.
+
+"""
+
+# This is built on the demo package as described at
+# https://packaging.python.org/en/latest/distributing.html
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Psynteract',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.6.1',
+
+    description='Psynteract Python bindings',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/psynteract/psynteract-py',
+
+    # Author details
+    author='Felix Henninger & Pascal Kieslich',
+    author_email='mailbox@felixhenninger.com',
+
+    # Choose your license
+    license='Apache 2.0',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: Apache 2.0',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['requests', 'pycouchdb'],
+)
